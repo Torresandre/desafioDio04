37de722dcc3e46ca6d7582b7fbb21d27e2dec719
André Torres <andretorresengm@hotmail.com>
2024-06-22 20:23:54 +0100

    desafio-04-desabilitando botão

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,9 @@
+import { ButtonContainer } from "./styles";
+import { IButtonProps } from "./types";
+
+const Button = ({ title, onClick, disabled }: IButtonProps) => {
+  return (
+    <ButtonContainer onClick={onClick} disabled={disabled}>{title}</ButtonContainer>);
+};
+
+export default Button;
